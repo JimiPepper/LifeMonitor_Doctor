14ae3deb481a6a73a44f448846000f629b2c4627
evanovertveldt <edmondvanovertveldt@live.fr>
2014-12-03 13:14:53 +0100

    EVT - 3/12 - init page

--- LifeMonitor_DoctorApp/test/spec/controllers/about.js
+++ LifeMonitor_DoctorApp/test/spec/controllers/about.js
@@ -1,30 +1,32 @@
-'use strict';
+
 
 // Unit test of controller patientsCtrl
 describe('Controller: patientsCtrl', function () {
 
-  var scope, mockedPatientsFactory, patientsCtrl;
+  var $scope,
+      $ctrl,
+      mockedPatientsFactory;
 
-  // load the controller's module
-  // Mock PatientsFactory
+  // load the module
   beforeEach(module('lifeMonitorDoctorApp', function($provide) {
+    //Mock PatientsFactory 
     mockedPatientsFactory = {
       getPatients: jasmine.createSpy()
     };
 
+    // Link Patients to mockedPatientsFactory
     $provide.value('Patients', mockedPatientsFactory);
   }));
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    patientsCtrl = $controller('patientsCtrl', {
-      $scope: scope
-    });
+
+  beforeEach(inject(function(_$rootScope_, _$controller_) {
+    $scope = _$rootScope_;
+    $ctrl = _$controller_('patientsCtrl', {$scope: $scope});
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    scope.loadPatients();
-    expect(mockedPatientsFactory.getPatients).toHaveBeenCalled();
+
+  it('should attach a list of awesomeThings to the scope', function() {
+      ctrl.loadPatients();
+      expect(mockedPatientsFactory.getPatients).toHaveBeenCalled();
   });
 });
